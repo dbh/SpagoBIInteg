3bf6a49f7fb75b66a271df3911e201edb232ba68
dbh <david@davidbharrison.com>
2015-05-11 17:09:49 -0700

    Switch over to logger

--- src/main/groovy/com/psionline/spagobi/GetListOfReports.groovy
+++ src/main/groovy/com/psionline/spagobi/GetListOfReports.groovy
@@ -1,13 +1,22 @@
 package com.psionline.spagobi; /**
  * Created by dharrison on 4/16/2015.
+ * David B. Harrison
  */
+
+//TODO Parameterize SpagoBI Environment
+//TODO Parameterize Document Type
+//TODO Parameterize Document Path in Functional Hierarchy
 
 import it.eng.spagobi.sdk.proxy.DocumentsServiceProxy;
 import it.eng.spagobi.sdk.documents.bo.SDKDocument;
-
 import java.rmi.RemoteException;
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
 public class GetListOfReports {
+    private static final Logger logger = LoggerFactory.getLogger("GetListOfReports")
+
     public static void main(String[] args) {
         String spagobiBaseURL = "http://localhost:8080/SpagoBI/sdk/DocumentsService";
         String spagobiUser = "biadmin";
@@ -15,23 +24,21 @@ public class GetListOfReports {
         DocumentsServiceProxy proxy = null;
         String docPath = null;
 
-        System.out.println("Main");
-//        docPath = "/Functionalities/Analytical documents/Reports/USPS_CLIENT_ADMIN";
-
+        logger.info("Main");
         try {
             proxy = new DocumentsServiceProxy(spagobiUser, spagobiPassword);
             proxy.setEndpoint(spagobiBaseURL);
 
             SDKDocument[] documents = proxy.getDocumentsAsList("REPORT", null, docPath);
             for (SDKDocument doc : documents) {
-                System.out.println(doc.getId() + ", "+ doc.getLabel()+", "+doc.getDescription());
+                logger.info(doc.getId() + ", "+ doc.getLabel()+", "+doc.getDescription());
             }
         }
         catch (RemoteException reE) {
-            System.out.println(reE);
+            logger.error(reE.getMessage());
         }
         catch (Exception e) {
-            System.out.println(e);
+            logger.info(e.getMessage());
         }
 
     }
